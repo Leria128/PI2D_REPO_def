c31b80aa9eb640317a689ee50f09623800d9e532
Claramh-13 <claramonasterioh@gmail.com>
2026-02-12 22:18:54 +0100

    panel y mensaje hecho

--- Proyecto_Integradodo/Assets/_PI2D_Root/Scripts/PanelCombinación.cs
+++ Proyecto_Integradodo/Assets/_PI2D_Root/Scripts/PanelCombinación.cs
@@ -1,12 +1,18 @@
 using DialogueSystem_V;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PanelCombinación : MonoBehaviour
 {
     [SerializeField] private string correctCode = "1997";
     [SerializeField] private Door door;
+
+    [Header("UI Mensajes")]
+    [SerializeField] private GameObject mensajePanel;
+    [SerializeField] private TMP_Text mensajeTexto;
+
     private string currentInput = "";
 
     public void AddNumber(string number)
@@ -17,12 +23,33 @@ public class PanelCombinación : MonoBehaviour
         {
             if (currentInput == correctCode)
             {
+                MostrarMensaje("Unlocked door");
                 door.UnlockDoor();
             }
+            else
+            {
+                {
+                    MostrarMensaje("Incorrect. Try Again");
+                }
+            }
 
-            currentInput = "";
+                currentInput = "";
         }
     }
+
+    private void MostrarMensaje(string texto)
+    {
+        mensajeTexto.text = texto;
+        mensajePanel.SetActive(true);
+        CancelInvoke(nameof(OcultarMensaje));
+        Invoke(nameof(OcultarMensaje), 2f);
+    }
+
+    private void OcultarMensaje()
+    {
+        mensajePanel.SetActive(false);
+    }
+
 
 
 
